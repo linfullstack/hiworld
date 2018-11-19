6137ff872b5ef15678ff05a2de75c202ee8ff580
asus <1046241014@qq.com>
2018-11-19 14:31:37 +0800

    test3

--- src/Hiworld.java
+++ src/Hiworld.java
@@ -1,6 +1,6 @@
 public class Hiworld {
     public static void main(String[] args){
         System.out.print("hi world");
-        System.out.print("china is great");
+        System.out.print("china is greatnihaoya");
     }
 }
